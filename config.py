900f640941c48932abda807b57887ee0c9e35c05
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-06-18 15:28:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
-MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("6369593642:AAGOrY98vY5dhNweA5jZqaj4xibG5t5VQr8")
+OPENAI_API_KEY = os.getenv("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")
+ADMIN_ID = int(os.getenv("6151769431"))
+LOG_CHANNEL = os.getenv("-1001978482047")
+MONGO_URI = os.getenv("mongodb+srv://harisin2008s:4srchZMgomsbS1QH@cluster0.9lfi56r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
