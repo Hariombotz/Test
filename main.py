1858f5817c349ad6712286004f626816f3d6b195
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-06-18 15:49:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def send_welcome(message: types.Message):
-    await message.reply("Hey bestie! 💖 I'm Ananya, your BFF chatbot!
-How are you feeling today?")
+    await message.reply("""Hey bestie! 💖 I'm Ananya, your BFF chatbot!""")
 
 @dp.message_handler(commands=["mood"])
 async def mood_checkin(message: types.Message):
