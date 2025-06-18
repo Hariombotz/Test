4060d1b70740665deb208134676f686660f27924
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-06-18 16:01:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")Add commentMore actions
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
-MONGO_URI = os.getenv("MONGO_URI")
